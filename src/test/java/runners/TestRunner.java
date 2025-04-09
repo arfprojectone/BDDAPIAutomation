c4d42c802498aac76337a7cb3eba27c1963d0274
Arif Kurniawan <arif.kurniawan@amartha.com>
2025-04-09 14:26:20 +0700

    add gitignore

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -8,10 +8,13 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(
         features = "src/test/resources/features", // Path ke file feature
         glue = "stepDefinitions", // Path ke step definitions
-        plugin = {"pretty", "html:target/cucumber-reports.html",
-                "json:target/cucumber.json"},
+        plugin = {"pretty",
+                "html:target/cucumber-reports.html",
+                "json:target/cucumber.json"
+        },
         monochrome = true // Output lebih rapi
 //        tags = "@User" // Bisa disesuaikan dengan tag pada feature file
+
 )
 public class TestRunner {
 }
